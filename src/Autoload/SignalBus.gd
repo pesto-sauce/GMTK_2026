e4f8ab448e601d747e05fc3664347f6234287baa
XpolioN <146839835+XpolioN2005@users.noreply.github.com>
2026-07-23 18:44:59 +0530

    feat: Add item drop and pickup system

---
+++ src/Autoload/SignalBus.gd
@@ -0,0 +1,9 @@
+extends Node
+
+@warning_ignore_start("unused_signal")
+
+signal item_add_to_inv(item: String, amount: int)
+signal item_remove_from_inv(item: String, amount: int)
+
+
+@warning_ignore_restore("unused_signal")
