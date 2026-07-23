f0ae73af4f319d2015bfa8456b1e278526026a73
XpolioN <146839835+XpolioN2005@users.noreply.github.com>
2026-07-23 18:49:50 +0530

    feat: Add configurable item drop offset and prevent multiple pickups

--- src/Scripts/item_drop.gd
+++ src/Scripts/item_drop.gd
@@ -1,38 +1,48 @@
 extends Node2D
 class_name ItemDrop
 
+
 const ITEM_DROP_SCENE: PackedScene = preload("res://Scenes/item_drop.tscn")
 const MIN_MOVE_SPEED: float = 50.0
 const PICKUP_THRESHOLD: float = 5.0
+
 
 @export var item_drops: Dictionary[StringName, Texture2D] = {
 	&"wood": null,
 	&"stone": null
 }
 
+
 # Item configuration
 var item: StringName
 var amount: int = 1
 var pickup_distance: float = 100.0
 var move_speed: float = 200.0
+var random_offset: float = 20.0
 
 var collected: bool = false
+
 
 @onready var sprite: Sprite2D = %sprite
 
 var player: CharacterBody2D
 
-static func setup(item_name: StringName, global_pos: Vector2, item_count: int = 1, distance: float = 100.0, speed: float = 200.0) -> ItemDrop:
+
+static func setup(item_name: StringName, global_pos: Vector2, item_count: int = 1, distance: float = 100.0, speed: float = 200.0, offset: float = 20.0) -> ItemDrop:
 	var drop: ItemDrop = ITEM_DROP_SCENE.instantiate()
 
 	drop.item = item_name
-
-	var random_offset: Vector2 = Vector2(randf_range(-20.0, 20.0), randf_range(-20.0, 20.0))
-	drop.global_position = global_pos + random_offset
-
 	drop.amount = item_count
 	drop.pickup_distance = distance
 	drop.move_speed = speed
+	drop.random_offset = offset
+
+	var random_position_offset: Vector2 = Vector2(
+		randf_range(-offset, offset),
+		randf_range(-offset, offset)
+	)
+
+	drop.global_position = global_pos + random_position_offset
 
 	return drop
 
@@ -61,16 +71,16 @@ func _physics_process(delta: float) -> void:
 		current_speed * delta
 	)
 
-	print("dist:", distance)
-	print("thereshold:", PICKUP_THRESHOLD)
-
 	if distance <= PICKUP_THRESHOLD and not collected:
-		print("emitted")
 		pickup()
 
 
 # Adds the item to the inventory and removes the drop.
 func pickup() -> void:
-	SignalBus.item_add_to_inv.emit(item, amount)
+	if collected:
+		return
+
 	collected = true
+
+	SignalBus.item_add_to_inv.emit(item, amount)
 	call_deferred("queue_free")
