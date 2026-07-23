11e20281093adb69329c4d980705115014634778
Yernoby <ybonreylouiejaugan@gmail.com>
2026-07-23 22:24:23 +0800

    lodge upgrade

--- src/Scripts/player.gd
+++ src/Scripts/player.gd
@@ -2,6 +2,10 @@ extends CharacterBody2D
 
 const SPEED = 300.0
 
+func _ready() -> void:
+	#signalbus.player_died.connect(_on_player_died)
+	pass
+
 func _physics_process(delta: float) -> void:
 	var direction := Input.get_vector("move_left", "move_right", "move_up", "move_down").normalized()
 	if direction:
