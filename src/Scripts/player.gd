15e85b8774f274b4d1e9f087ccabc4102196e016
Yernoby <ybonreylouiejaugan@gmail.com>
2026-07-23 20:05:47 +0800

    player movement
    wasd top down

---
+++ src/Scripts/player.gd
@@ -0,0 +1,12 @@
+extends CharacterBody2D
+
+const SPEED = 300.0
+
+func _physics_process(delta: float) -> void:
+	var direction := Input.get_vector("move_left", "move_right", "move_up", "move_down").normalized()
+	if direction:
+		velocity = direction * SPEED
+	else:
+		velocity = velocity.move_toward(Vector2.ZERO, SPEED)
+
+	move_and_slide()
