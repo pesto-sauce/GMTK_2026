e4228b8ce08a560631602b0e9922c8fce9ddb58c
XpolioN <146839835+XpolioN2005@users.noreply.github.com>
2026-07-24 17:56:41 +0530

    fix: Emit current day with day_changed signal

--- src/Scripts/day_night.gd
+++ src/Scripts/day_night.gd
@@ -17,7 +17,7 @@ func _process(delta: float) -> void:
 		time -= 1.0
 		day += 1
 
-		SignalBus.day_changed.emit(1)
+		SignalBus.day_changed.emit(day)
 
 	# Send time to GameManager in 0-24 hour scale
 	GameManager.time = time * 24.0
